347763cf46c4e87f9e2b233702db9f9ed6f8b471
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-03-12 22:54:48 +0530

    error created

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const WebSearchPage = () => {
-  return <div>WebSearchPage</div>;
-};
+export default async function page({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm} `
+  );
+  if (!response.ok) throw new Error("Something went wrong");
+  const data = await response.json();
+  const results = data.items;
 
-export default WebSearchPage;
+  if (!results) {
+    return (
+      <div className=" flex flex-col justify-center items-center pt-10">
+        <h1 className=" text-lg mb-5">
+          Your search -
+          <span className="  font-bold"> {searchParams.searchTerm}</span> - did
+          not match any documents.
+        </h1>
+        <h1>Suggestions:</h1>
+        <ul style="list-style-type:circle">
+          <li>Make sure that all words are spelled correctly.</li>
+          <li>Try different keywords.</li>
+          <li>Try more general keywords.</li>
+        </ul>
+      </div>
+    );
+  }
+}
