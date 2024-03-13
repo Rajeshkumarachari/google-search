b44f5a330eb08c5d6bf3b688a70921a82789dd61
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-03-13 10:03:43 +0530

    add image results

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,7 +1,31 @@
+import ImageSearchResults from "@/components/ImageSearchResults";
 import React from "react";
 
-const ImageSearchPage = () => {
-  return <div className="">ImageSearchPage</div>;
-};
+export default async function ImageSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
+  if (!response.ok) throw new Error("Something went wrong");
+  const data = await response.json();
+  const results = data.items;
 
-export default ImageSearchPage;
+  if (!results) {
+    return (
+      <div className=" flex flex-col justify-center items-center pt-10">
+        <h1 className=" text-lg mb-5">
+          Your search -
+          <span className="  font-bold"> {searchParams.searchTerm}</span> - did
+          not match any documents.
+        </h1>
+        <h1>Suggestions:</h1>
+        <ul style="list-style-type:circle">
+          <li>Make sure that all words are spelled correctly.</li>
+          <li>Try different keywords.</li>
+          <li>Try more general keywords.</li>
+        </ul>
+      </div>
+    );
+  }
+
+  return <div> {results && <ImageSearchResults results={data} />} </div>;
+}
